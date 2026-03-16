7e8fb47d4256634b225b730d2c5183a6547597d8
Ludovic Courtin <ludovic.courtin@etu.univ-tours.fr>
2026-03-16 11:31:10 +0100

    LC - Commit 2 : Mise en place des callbacks pour la page 1

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
 from pages.table import layout
+import pages.table_cb
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = layout
 
 if __name__ == "__main__":
     app.run(debug=True)
+
---
+++ pages/table_cb.py
@@ -0,0 +1,17 @@
+from dash import callback, Input, Output
+import pandas as pd
+import pathlib
+
+PATH = pathlib.Path(__file__).parent.parent
+df = pd.read_csv(PATH / 'datas' / 'avocado.csv')
+
+@callback(
+    Output("avocado-table", "data"),
+    Input("region-dropdown", "value"),
+    Input("type-dropdown", "value"),
+)
+def update_table(region, type_):
+    df_filtered = df[df["region"] == region]
+    if type_ != "Tous":
+        df_filtered = df_filtered[df_filtered["type"] == type_]
+    return df_filtered.to_dict("records")
